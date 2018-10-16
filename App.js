2847815ccc6daa641744b1e4f953db1212ad640d
Adrian (Lloyd) Flanagan <aflanagan@bhmginc.com>
2018-10-16 12:50:26 -0400

    Sigh. Giving up on react-native CLI and using expo.

--- App.js
+++ App.js
@@ -1,67 +1,21 @@
-/**
- * All My Stuff Mobile Client
- * https://github.com/alflanagan
- *
- * @format
- * @flow
- */
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
-/*
-  * Copyright 2018 by Adrian L. Flanagan
-  *  This file is part of All My Stuff Mobile Client.
-  *
-  *  All My Stuff is free software: you can redistribute it and/or modify
-  *  it under the terms of the GNU General Public License as published by
-  *  the Free Software Foundation, either version 3 of the License, or
-  *  (at your option) any later version.
-  *
-  *  All My Stuff is distributed in the hope that it will be useful,
-  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  *  GNU General Public License for more details.
-  *
-  *  You should have received a copy of the GNU General Public License
-  *  along with All My Stuff.  If not, see <https://www.gnu.org/licenses/>.
-  */
-
-import React, { Component } from 'react' // eslint-disable-line no-unused-vars
-import { Platform, StyleSheet, Text, View } from 'react-native' // eslint-disable-line no-unused-vars
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu'
-})
-
-type Props = {};
-export default class App extends Component<Props> {
-  render () {
+export default class App extends React.Component {
+  render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text>Open up App.js to start working on your app!</Text>
       </View>
-    )
+    );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    backgroundColor: '#fff',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF'
+    justifyContent: 'center',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5
-  }
-})
+});
